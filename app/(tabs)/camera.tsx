70c8f50cd74cbcee7ccb934142671e9c3b9de423
rikardbq <j10rikardb@gmail.com>
2025-02-20 15:52:25 +0100

    add all the stuff

---
+++ app/(tabs)/camera.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useRef, useState } from "react";
+import {
+    Button,
+    Image,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import {
+    Camera,
+    useCameraDevice,
+    useCameraPermission,
+    useMicrophonePermission,
+} from "react-native-vision-camera";
+import { useDoubleTap } from "../../hooks/useDoubleTap";
+
+export default function CameraTab() {
+    const camera = useRef<Camera>(null);
+    const device = useCameraDevice("back");
+    const {
+        hasPermission: hasCamPermission,
+        requestPermission: requestCamPermission,
+    } = useCameraPermission();
+    const {
+        hasPermission: hasMicPermission,
+        requestPermission: requestMicPermission,
+    } = useMicrophonePermission();
+    const { isDoubleTap, doubleTapHandler } = useDoubleTap();
+    const [photo, setPhoto]: any = useState(null);
+    const [isInit, setInit] = useState(false);
+
+    useEffect(() => {
+        if (isInit && isDoubleTap) {
+            (async () => {
+                const file = await camera.current?.takePhoto();
+                setPhoto(file);
+            })();
+        }
+    }, [isDoubleTap, isInit]);
+
+    if (!hasCamPermission) requestCamPermission();
+    if (!hasMicPermission) requestMicPermission();
+    if (device == null) return <View>ERROR</View>;
+
+    return (
+        <>
+            <TouchableOpacity
+                style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: "50%",
+                    backgroundColor: "#e3e3e3",
+                    opacity: 0.75,
+                    position: "absolute",
+                    bottom: 50,
+                    alignSelf: "center",
+                    zIndex: 999,
+                }}
+            ></TouchableOpacity>
+            <Camera
+                onTouchStart={doubleTapHandler}
+                ref={camera}
+                style={StyleSheet.absoluteFill}
+                device={device}
+                isActive={true}
+                photo={true}
+                video={true}
+                onInitialized={() => {
+                    setInit(true);
+                }}
+            />
+        </>
+    );
+}
